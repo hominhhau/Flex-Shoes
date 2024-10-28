ed14bfc4f1786e63caf015199463fef74061bf36
Thùy Nhi IT <21054781.nhi@student.iuh.edu.vn>
2024-10-28 23:05:26 +0700

    Hoàn thành mục productDetail customize từ ananas

--- frontend/src/config/routes.jsx
+++ frontend/src/config/routes.jsx
@@ -1,5 +1,6 @@
 const routes = {
     home: '/',
+    productdetail: '/productdetail',
     search: '/search',
 };
 
--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -1,10 +1,11 @@
 import Home from '../pages/Home';
 import Search from '../pages/Search';
-
+import ProductDetail from '../pages/productdetail';
 import config from '../config';
 
 const publicRoutes = [
     { path: config.routes.home, component: Home },
+    {path:config.routes.productdetail,component: ProductDetail},
     { path: config.routes.search, component: Search },
     // { path: config.routes.search, component: Search, layout: null },
 ];
